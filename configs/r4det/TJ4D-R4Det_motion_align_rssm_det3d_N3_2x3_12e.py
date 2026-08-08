d97aa7bf0b0f5db582a0609e00a5272f0bd59f10
SU0510 <lurui0123@126.com>
2026-08-08 16:30:33 +0000

    feat: add gradient accumulation cumulative_iters=2, lr=0.0003

--- configs/r4det/TJ4D-R4Det_motion_align_rssm_det3d_N3_2x3_12e.py
+++ configs/r4det/TJ4D-R4Det_motion_align_rssm_det3d_N3_2x3_12e.py
@@ -401,10 +401,10 @@ data = dict(
         seq_len=3))
 
 # Training settings
-lr = 0.00015
+lr = 0.0003
 max_epochs = 18
 optimizer = dict(type='AdamW', lr=lr, betas=(0.95, 0.99), weight_decay=0.01)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2), cumulative_iters=2)
 runner = dict(type='EpochBasedRunner', max_epochs=max_epochs)
 lr_config = dict(
     policy='CosineAnnealing',
