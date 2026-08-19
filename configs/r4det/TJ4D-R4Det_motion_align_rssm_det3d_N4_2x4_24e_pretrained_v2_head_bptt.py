5fc50dfee3075052619baf8719d9afc7dbf9ef40
SU0510 <lurui0123@126.com>
2026-08-19 12:49:38 +0000

    fix: reduce samples_per_gpu 3->2 to fit BPTT memory on 3 GPUs

--- configs/r4det/TJ4D-R4Det_motion_align_rssm_det3d_N4_2x4_24e_pretrained_v2_head_bptt.py
+++ configs/r4det/TJ4D-R4Det_motion_align_rssm_det3d_N4_2x4_24e_pretrained_v2_head_bptt.py
@@ -386,7 +386,7 @@ eval_pipeline = test_pipeline
 
 # dataset settings
 data = dict(
-    samples_per_gpu=3,
+    samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type='RepeatDataset',
